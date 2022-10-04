bd659fd351adf9e1c436f45de1414e52fddfbeac
name-myatnoe <eaintmyatnoe0517@gmail.com>
2022-10-04 15:50:49 +0630

    update

---
+++ src/actors/enemy.gd
@@ -0,0 +1,19 @@
+extends action
+
+func _ready():
+	set_physics_process(false)
+	velocity.x = -speed.x
+	
+#func _on_StompDetector_body_entered(body):
+	#if body.global_position.y > get_node("StompDetector").global_position.y:
+		
+	
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	if is_on_wall():
+		velocity.x *= -1
+	velocity.y = move_and_slide(velocity,normal_floor).y
+
+
+
+	
---
+++ src/levels/moving_actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+class_name action
+
+export var speed := Vector2(300,1000)
+export var gravity :=5000
+var velocity := Vector2.ZERO
+var normal_floor := Vector2.UP
+
+
