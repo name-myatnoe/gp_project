c17f3f7e12948dd20c894b8e6729ee5e4cfe3de9
Dia1529 <saraslays365@gmail.com>
2022-10-16 23:05:10 +0630

    #added animated coins and tiles
    no more deleting our scenes github-chan pls.

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -1,7 +1,11 @@
-extends Node2D
+extends Area2D
 
 
-onready var anim_player : AnimationPlayer= get_node("AnimationPlayer")
+onready var anim_player : AnimationPlayer = get_node("AnimationPlayer ")
 
 
 
+
+
+func _on_Coin_body_entered(_body :PhysicsBody2D ) -> void:
+	anim_player.play ("fade")
