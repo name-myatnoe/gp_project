c5780fe328e78267e25bc922a1f41dff98f1c792
Dia1529 <saraslays365@gmail.com>
2022-10-30 23:15:57 +0630

    menu added
    menu added

---
+++ src/levels/Menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"VBoxContainer1/Start Button".grab_focus()
+	
+	
+
+
+
+
+func _on_Start_Button_pressed():
+		get_tree().change_scene("res://src/levels/Testkzt.tscn")
+
+
+func _on_Bye_pressed():
+	get_tree().quit() 
+
+
+func _on_Map_pressed():
+	get_tree().change_scene("res://src/levels/Map.tscn")
