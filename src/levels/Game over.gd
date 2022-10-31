839e26fc2e3d56d735e75efc7cd00782c082e2e0
Dia1529 <saraslays365@gmail.com>
2022-10-31 21:08:46 +0630

    #menu+scenes
    menu + win+ lose scene

---
+++ src/levels/Game over.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer2.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Main_menu_pressed():
+	get_tree().change_scene("res://src/levels/Menu.tscn")
+
+
+func _on_again_pressed():
+	get_tree().change_scene("res://src/levels/Testkzt.tscn")
