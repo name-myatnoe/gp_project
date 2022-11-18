e587802bc7bd9a2479c2667a09ffd6e7c2dc712b
Dia1529 <saraslays365@gmail.com>
2022-11-18 14:15:12 +0630

    dragon sound added+ coin fix + dragon position changed

--- Coin.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-
-func _on_Coin_body_entered(body):
-	$collect.play()
-
-func _on_Coin_body_exited(body):
-	queue_free()
--- src/levels/Coin.gd
+++ src/levels/Coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-
-
-
 func _on_Coin_body_entered(body):
 	$collect.play()
+	$Timer.start()
+
+func _on_Timer_timeout():
+	queue_free()
